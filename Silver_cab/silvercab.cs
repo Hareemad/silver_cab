3e85281538a6b7722a8beab033d6639c1a9cfaa3
hareemad <hareemssuet@gmail.com>
2024-05-03 16:06:43 +0500

    COMMENTS

--- Silver_cab/silvercab.cs
+++ Silver_cab/silvercab.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace silver_cab
 {
-    public partial class Form1 : Form
+    public partial class silvercab : Form
     {
-        public Form1()
+        public silvercab()
         {
             InitializeComponent();
         }
